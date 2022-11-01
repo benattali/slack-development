e5630eaed62746590cea8d9d5253743f2efbbd1e
Tanya <homynyk@ualberta.ca>
2022-10-31 18:07:02 -0700

    Started adding user prompts

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ async function initializeRules() {
 }
 
 // example of creating a new route if we need to
-// receiver.router.get('/test', (req, res) => {
-//   res.send('yay!')
-// });
+receiver.router.post('/create-rule', (req, res) => {
+  console.log("********")
+});
 
 app.command('/showrules', async ({ command, ack, say }) => {
   // Acknowledge command request
@@ -43,24 +43,77 @@ app.command('/showrules', async ({ command, ack, say }) => {
 
 })
 
-app.command('/addrule', async ({ command, ack, say }) => {
+app.command('/addrule', async ({ ack, body, client, logger }) => {
   await ack()
 
-  // TODO: set up user prompts for keywords and channel
-  const textArr = command.text.split(" ")
-  const keyword = textArr[0]
-  const channelToSend = textArr[1].match(/([A-Z])\w+/)[0]
-  const channelNameToSend = textArr[1].match(/(?!\|)([a-z])+/)[0]
-  const rule = new Rule({
-    listeningChannelId: command.channel_id,
-    echoToChannelId: channelToSend,
-    keyword: keyword,
-    echoToChannelName: channelNameToSend,
-  })
-  await rule.save()
-  setListener(app, rule)
+  try {
+    const result = await client.views.open({
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'input_c',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
 
-  await say(`Added ${rule.keyword} ---> ${rule.echoToChannelName}`)
+    logger.info("****", result);
+  } catch (e) {
+    logger.error(error);
+  }
+  logger.info()
+
+  // const textArr = command.text.split(" ")
+  // const keyword = textArr[0]
+  // const channelToSend = textArr[1].match(/([A-Z])\w+/)[0]
+  // const channelNameToSend = textArr[1].match(/(?!\|)([a-z])+/)[0]
+  // const rule = new Rule({
+  //   listeningChannelId: command.channel_id,
+  //   echoToChannelId: channelToSend,
+  //   keyword: keyword,
+  //   echoToChannelName: channelNameToSend,
+  // })
+  // await rule.save()
+  // setListener(app, rule)
 });
 
 (async () => {
